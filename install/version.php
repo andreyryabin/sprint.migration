3a7d21a09b6355785e03fc4de81c0ccbe06d1475
Andrey Ryabin <andreyryabin@yandex.ru>
2015-01-20 05:38:08 +0500

    version 2.0.0 description.ru

--- install/version.php
+++ install/version.php
@@ -1,7 +1,7 @@
 <?php
 
 $arModuleVersion = array(
-    "VERSION" => "1.0.0",
+    "VERSION" => "2.0.0",
     "VERSION_DATE" => "2015-01-18 12:00:00",
     "MODULE_NAME" => "Спринт.Миграции",
     "MODULE_DESCRIPTION" => "Модуль для миграций изменений бд",
