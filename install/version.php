f7423d00e907ce6c19b19d64c0299d21aec2cdde
Andrey Ryabin <andreyryabin@yandex.ru>
2015-03-01 16:26:47 +0500

    version

--- install/version.php
+++ install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = array(
-    "VERSION" => "2.0.1",
-    "VERSION_DATE" => "2015-02-05 12:00:00",
+    "VERSION" => "2.0.2",
+    "VERSION_DATE" => "2015-03-01 12:00:00",
 );
