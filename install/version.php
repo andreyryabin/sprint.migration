81f3946a6ebf0637dbd602de2dceeaf286f2a7de
Andrey Ryabin <andreyryabin@yandex.ru>
2015-06-01 02:23:03 +0500

    версия 2.1.1

--- install/version.php
+++ install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = array(
-    "VERSION" => "2.0.5",
-    "VERSION_DATE" => "2015-05-18 12:00:00",
+    "VERSION" => "2.1.1",
+    "VERSION_DATE" => "2015-05-30 12:00:00",
 );
