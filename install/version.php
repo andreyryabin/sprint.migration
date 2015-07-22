1a67d00c0ea70c53c215584f884d24b703e8636b
Andrey Ryabin <andreyryabin@yandex.ru>
2015-07-22 13:04:31 +0500

    marketplace version 2.2.2

--- install/version.php
+++ install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = array(
-    "VERSION" => "2.2.1",
-    "VERSION_DATE" => "2015-06-22 12:00:00",
+    "VERSION" => "2.2.2",
+    "VERSION_DATE" => "2015-07-22 12:00:00",
 );
