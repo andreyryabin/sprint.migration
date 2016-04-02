87262fa497734c30c48d362a0080ef5775f6f44b
Andrey Ryabin <andrey.ryabin@gmail.com>
2016-04-02 21:47:53 +0500

    версия 2.2.8

--- install/version.php
+++ install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = array(
-    "VERSION" => "2.2.7",
-    "VERSION_DATE" => "2016-01-15 12:00:00",
+    "VERSION" => "2.2.8",
+    "VERSION_DATE" => "2016-04-02 12:00:00",
 );
