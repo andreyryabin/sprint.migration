63a7270437a7b2400232c75bddfc0863e2cf2373
Andrey Ryabin <andreyryabin@yandex.ru>
2018-08-28 04:36:35 +0500

    release-2.7.1

--- loader.php
+++ loader.php
@@ -12,8 +12,8 @@ spl_autoload_register(function ($className) {
             $className = substr($className, $lastNsPos + 1);
             $fileName = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
         }
-        $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
 
+        $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
         $fileName = ($includePath !== null ? $includePath . DIRECTORY_SEPARATOR : '') . $fileName;
         if (is_readable($fileName)) {
             /** @noinspection PhpIncludeInspection */
