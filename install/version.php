f04cd87800f22b2103a1489ba57a95271a7ca240
Andrey Ryabin <andreyryabin@yandex.ru>
2018-10-12 02:17:02 +0500

    2.8.7

--- install/version.php
+++ install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = array(
-    "VERSION" => "2.8.6",
-    "VERSION_DATE" => "2018-09-20 12:00:00",
+    "VERSION" => "2.8.7",
+    "VERSION_DATE" => "2018-10-11 12:00:00",
 );
