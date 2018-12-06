e00a033ecff10ddda567f7c124816515adbcb399
Andrey Ryabin <andreyryabin@yandex.ru>
2018-12-07 02:49:02 +0500

    2.9

--- admin/includes/version.php
+++ admin/includes/version.php
@@ -14,29 +14,30 @@
     </div>
 
     <div class="sp-group">
-        <div class="sp-group-row1">
+        <div class="sp-group-row2">
             <div class="sp-block">
                 <input type="button" value="<?= GetMessage('SPRINT_MIGRATION_UP_START') ?>"
                        onclick="migrationMigrationsUpConfirm();" class="adm-btn-green"/>
                 <input type="button" value="<?= GetMessage('SPRINT_MIGRATION_DOWN_START') ?>"
                        onclick="migrationMigrationsDownConfirm();"/>
-                <div class="sp-filter">
-                    <? $search = \Sprint\Migration\Module::getDbOption('admin_versions_search', ''); ?>
-                    <input placeholder="<?= GetMessage('SPRINT_MIGRATION_SEARCH') ?>" style="" type="text"
-                           value="<?= $search ?>" class="adm-input" name="migration_search"/>
-                    <? $view = \Sprint\Migration\Module::getDbOption('admin_versions_view', 'list'); ?>
-                    <select class="sp-stat">
-                        <option <? if ($view == 'list'): ?>selected="selected"<? endif ?>
-                                value="list"><?= GetMessage('SPRINT_MIGRATION_TOGGLE_LIST') ?></option>
-                        <option <? if ($view == 'new'): ?>selected="selected"<? endif ?>
-                                value="new"><?= GetMessage('SPRINT_MIGRATION_TOGGLE_NEW') ?></option>
-                        <option <? if ($view == 'installed'): ?>selected="selected"<? endif ?>
-                                value="installed"><?= GetMessage('SPRINT_MIGRATION_TOGGLE_INSTALLED') ?></option>
-                        <option <? if ($view == 'status'): ?>selected="selected"<? endif ?>
-                                value="status"><?= GetMessage('SPRINT_MIGRATION_TOGGLE_STATUS') ?></option>
-                    </select>
-                    <input type="button" value="<?= GetMessage('SPRINT_MIGRATION_SEARCH') ?>" class="sp-search"/>
-                </div>
+            </div>
+            <div class="sp-block">
+                <? $search = \Sprint\Migration\Module::getDbOption('admin_versions_search', ''); ?>
+                <input placeholder="<?= GetMessage('SPRINT_MIGRATION_SEARCH') ?>" style="" type="text"
+                       value="<?= $search ?>" class="adm-input" name="migration_search"/>
+                <? $view = \Sprint\Migration\Module::getDbOption('admin_versions_view', 'list'); ?>
+                <select class="sp-stat">
+                    <option <? if ($view == 'list'): ?>selected="selected"<? endif ?>
+                            value="list"><?= GetMessage('SPRINT_MIGRATION_TOGGLE_LIST') ?></option>
+                    <option <? if ($view == 'new'): ?>selected="selected"<? endif ?>
+                            value="new"><?= GetMessage('SPRINT_MIGRATION_TOGGLE_NEW') ?></option>
+                    <option <? if ($view == 'installed'): ?>selected="selected"<? endif ?>
+                            value="installed"><?= GetMessage('SPRINT_MIGRATION_TOGGLE_INSTALLED') ?></option>
+                    <option <? if ($view == 'status'): ?>selected="selected"<? endif ?>
+                            value="status"><?= GetMessage('SPRINT_MIGRATION_TOGGLE_STATUS') ?></option>
+                </select>
+                <input type="button" value="<?= GetMessage('SPRINT_MIGRATION_SEARCH') ?>" class="sp-search"/>
+
             </div>
         </div>
     </div>
