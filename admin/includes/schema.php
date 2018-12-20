b6dab6c25f6b18d43c37ae2a1425a5cec4f551fa
Andrey Ryabin <andreyryabin@yandex.ru>
2018-12-20 07:13:59 +0500

    3.0

--- admin/includes/schema.php
+++ admin/includes/schema.php
@@ -17,7 +17,7 @@
                 <input type="button" value="<?= GetMessage('SPRINT_MIGRATION_SELECT_ALL') ?>" class="sp-schema-check"/>
             </div>
             <div class="sp-block">
-                <input type="button" value="<?= GetMessage('SPRINT_MIGRATION_SCHEMA_IMPORT') ?>" class="sp-schema-import adm-btn-green"/>
+                <input type="button" value="<?= GetMessage('SPRINT_MIGRATION_SCHEMA_IMPORT') ?>" class="sp-schema-import"/>
                 <input type="button" value="<?= GetMessage('SPRINT_MIGRATION_SCHEMA_EXPORT') ?>" class="sp-schema-export"/>
                 <div style="width: 300px;display: inline-block;margin: 0 10px;">
                     <div id="schema_progress_current"><span class="bar"></span></div>
