430865cbb603d757d270dbb3b745ec342038c870
Andrey Ryabin <andreyryabin@yandex.ru>
2019-03-11 04:04:28 +0500

    3.0.14

--- install/version.php
+++ install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = array(
-    "VERSION" => "3.0.12",
-    "VERSION_DATE" => "2010-02-22 12:00:10",
+    "VERSION" => "3.0.14",
+    "VERSION_DATE" => "2010-03-10 12:00:10",
 );
