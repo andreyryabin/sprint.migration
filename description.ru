11659cbce78fefa5d00737d8fb9d5421cc59e8d7
Andrey Ryabin <andreyryabin@yandex.ru>
2019-09-23 19:56:18 +0500

    3.6.9

--- description.ru
+++ description.ru
@@ -1,1 +1,2 @@
 1) ѕроверка создани€ директорий модулем (автор Konstantin Sumarokov) <br/>
+1) ѕеренос статусов форм (автор Konstantin Sumarokov) <br/>
--- install/version.php
+++ install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = [
-    "VERSION" => "3.6.8",
-    "VERSION_DATE" => "2019-09-22 12:00:00",
+    "VERSION" => "3.6.9",
+    "VERSION_DATE" => "2019-09-22 14:00:00",
 ];
