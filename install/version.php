2a952f380ac1309d6aa42245e22ca0afca2dfcf8
Andrey Ryabin <andreyryabin@yandex.ru>
2020-01-30 20:12:42 +0500

    3.12.19

--- install/version.php
+++ install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = [
-    "VERSION" => "3.12.18",
-    "VERSION_DATE" => "2020-01-30 12:20:20",
+    "VERSION" => "3.12.19",
+    "VERSION_DATE" => "2020-01-30 14:00:00",
 ];
