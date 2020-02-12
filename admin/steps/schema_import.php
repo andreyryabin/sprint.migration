2a94cd43a8e3372ba10e7d322006ecaf4339224d
Andrey Ryabin <andreyryabin@yandex.ru>
2020-02-12 17:54:46 +0500

    3.13.5

--- admin/steps/schema_import.php
+++ admin/steps/schema_import.php
@@ -5,23 +5,24 @@ use Sprint\Migration\Out;
 use Sprint\Migration\SchemaManager;
 use Sprint\Migration\VersionConfig;
 
-if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
     die();
 }
 
 $hasSteps = (
-    ($_POST["step_code"] == "schema_import") ||
-    ($_POST["step_code"] == "schema_test")
+    ($_POST['step_code'] == 'schema_import') ||
+    ($_POST['step_code'] == 'schema_test')
 );
 
 if ($hasSteps && check_bitrix_sessid('send_sessid')) {
     $params = !empty($_POST['params']) ? $_POST['params'] : [];
     $checked = !empty($_POST['schema_checked']) ? $_POST['schema_checked'] : [];
+    $stepCode = !empty($_POST['step_code']) ? $_POST['step_code'] : '';
 
     /** @var $versionConfig VersionConfig */
     $schemaManager = new SchemaManager($versionConfig, $params);
 
-    if ($_POST["step_code"] == "schema_test") {
+    if ($stepCode == 'schema_test') {
         $schemaManager->setTestMode(1);
     } else {
         $schemaManager->setTestMode(0);
@@ -43,7 +44,7 @@ if ($hasSteps && check_bitrix_sessid('send_sessid')) {
 
         ?>
         <script>
-            schemaExecuteStep('<?=$_POST["step_code"]?>', <?=$json?>);
+            schemaExecuteStep('<?=$stepCode?>', <?=$json?>);
         </script>
         <?
     } catch (Exception $e) {
@@ -59,7 +60,7 @@ if ($hasSteps && check_bitrix_sessid('send_sessid')) {
     foreach ($progress as $type => $val) {
         ?>
         <script>
-            schemaProgress('<?=$type?>',<?=$val?>);
+            schemaProgress('<?=$type?>', <?=$val?>);
         </script>
         <?
     }
--- install/version.php
+++ install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = [
-    "VERSION" => "3.13.4",
-    "VERSION_DATE" => "2020-02-10 10:10:00",
+    "VERSION" => "3.13.5",
+    "VERSION_DATE" => "2020-02-10 10:20:00",
 ];
