3a12aea5c66d86b727c719bef84d8aaf5f99c1c6
Andrey Ryabin <andreyryabin@yandex.ru>
2024-05-11 18:36:27 +0500

    4.7.2

--- admin/pages/support.php
+++ admin/pages/support.php
@@ -5,11 +5,13 @@ use Sprint\Migration\Locale;
 
 global $APPLICATION;
 $APPLICATION->SetTitle(Locale::getMessage('MENU_SUPPORT'));
+$ts = date('YmdH');
+
 ?>
 <div id="support_page"></div>
 
-<link href="https://andreyryabin.github.io/sprint_migration/support.css" rel="stylesheet" type="text/css">
-<script src="https://andreyryabin.github.io/sprint_migration/support.js"></script>
+<link href="https://andreyryabin.github.io/sprint_migration/support.css?<?= $ts ?>" rel="stylesheet" type="text/css">
+<script src="https://andreyryabin.github.io/sprint_migration/support.js?<?= $ts ?>"></script>
 
 <?php
 include __DIR__ . '/../includes/help.php';
