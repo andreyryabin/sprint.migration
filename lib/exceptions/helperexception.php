707f81b553424d0d5887a006c2951215e7721c99
temi4sh <temi4sh@gmail.com>
2026-07-03 06:55:24 +0300

    Fix HelperException nullable previous parameter

--- lib/exceptions/helperexception.php
+++ lib/exceptions/helperexception.php
@@ -7,7 +7,7 @@ use Throwable;
 
 class HelperException extends Exception
 {
-    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    public function __construct($message = "", $code = 0, ?Throwable $previous = null)
     {
         $message = is_array($message) ? implode(', ', $message) : $message;
 
